b02dcffca3a13ffab84452fe5ee0b9a8d2ba89a0
Kirill Smirnov <kirill@dg-line.ru>
2020-09-08 11:30:06 +0300

    init repo

---
+++ Runtime/UnityUtils/MonoBehaviourNullCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace UnityUtils
+{
+    public static class MonoBehaviourNullCheck
+    {
+        /// <summary>
+        /// Checks non-static public and serialized fields of object. Prints warnings in log on null fields.
+        /// <para>Example usage:</para>
+        /// <para>OnValidate() => CheckNullFields(this)</para>
+        /// </summary>
+        public static void CheckNullFields(object obj)
+        {
+            var type = obj.GetType();
+            foreach (var field in type.GetRuntimeFields())
+            {
+                if (!field.IsStatic && (field.IsPublic || Attribute.IsDefined(field, typeof(SerializeField))))
+                {
+                    var val = field.GetValue(obj);
+                    if (val.IsNull())
+                    {
+                        Debug.LogWarning($"{obj.GetType().Name}: {field.Name} is null");
+                    }
+                }
+            }
+        }
+
+        public static bool IsNull(this object obj) => obj == null || obj.Equals(null);
+    }
+}
