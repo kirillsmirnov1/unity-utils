830075ebe96b55f850fcca092d407621c9827eb0
Kirill Smirnov <kirill@dg-line.ru>
2020-09-18 13:15:53 +0300

    LinqExtensions uses int.Maxvalue

--- Runtime/UnityUtils/LinqExtensions.cs
+++ Runtime/UnityUtils/LinqExtensions.cs
@@ -6,10 +6,7 @@ namespace UnityUtils
 {
     public static class LinqExtensions
     {
-        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> enumerable)
-        {
-            var maxRand = enumerable.Count() * 10;
-            return enumerable.OrderBy(x => Random.Range(0, maxRand));
-        }
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> enumerable) 
+            => enumerable.OrderBy(x => Random.Range(0, int.MaxValue));
     }
 }
