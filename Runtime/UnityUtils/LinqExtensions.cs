eb20a2ed93f50f81497f7254f663c907cdd51a1e
Kirill Smirnov <kirill@dg-line.ru>
2020-09-18 13:00:33 +0300

    add LinqExtensions

---
+++ Runtime/UnityUtils/LinqExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace UnityUtils
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> enumerable)
+        {
+            var maxRand = enumerable.Count() * 10;
+            return enumerable.OrderBy(x => Random.Range(0, maxRand));
+        }
+    }
+}
