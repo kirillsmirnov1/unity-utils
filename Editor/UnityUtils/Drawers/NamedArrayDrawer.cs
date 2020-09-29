68db8ff528bf0c04a7ecb3b6a2befdd72935a3b0
Kirill Smirnov <kirill@dg-line.ru>
2020-09-29 11:26:32 +0300

    Add NamedArray attribute

---
+++ Editor/UnityUtils/Drawers/NamedArrayDrawer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityUtils.Attributes;
+
+namespace UnityUtils.Drawers
+{
+    [CustomPropertyDrawer(typeof(NamedArrayAttribute))]
+    public class NamedArrayDrawer : PropertyDrawer {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
+            // Properly configure height for expanded contents.
+            return EditorGUI.GetPropertyHeight(property, label, property.isExpanded);
+        }
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            // Replace label with enum name if possible.
+            try {
+                var config = attribute as NamedArrayAttribute;
+                var enumNames = System.Enum.GetNames(config.TargetEnum);
+                var pos = int.Parse(property.propertyPath.Split('[', ']')[1]);
+                var enumLabel = enumNames.GetValue(pos) as string;
+                // Make names nicer to read (but won't exactly match enum definition).
+                enumLabel = ObjectNames.NicifyVariableName(enumLabel.ToLower());
+                label = new GUIContent(enumLabel);
+            } catch {
+                // keep default label
+            }
+            EditorGUI.PropertyField(position, property, label, property.isExpanded);
+        }
+    }
+}
---
+++ Runtime/UnityUtils/Attributes/NamedArrayAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityUtils.Attributes
+{
+    /// Defines an attribute that makes the array use enum values as labels.
+    /// Use like this:
+    ///      [NamedArray(typeof(eDirection))] public GameObject[] m_Directions;
+    public class NamedArrayAttribute : PropertyAttribute {
+        public readonly Type TargetEnum;
+        public NamedArrayAttribute(Type targetEnum) => TargetEnum = targetEnum;
+    }
+}
