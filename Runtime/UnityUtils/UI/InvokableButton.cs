a0b265b2696422132c7d01f9774de40eeb7b2a14
Kirill Smirnov <kirill@dg-line.ru>
2020-09-29 11:32:22 +0300

    add InvokableButton

---
+++ Runtime/UnityUtils/UI/InvokableButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityUtils.UI
+{
+    public class InvokableButton : MonoBehaviour
+    {
+        private Button _button;
+
+        private void Awake ()
+        {
+            if (_button == null)
+                _button = GetComponent<Button>();
+        }
+ 
+        public void Invoke()
+        {
+            if (_button != null)
+            {
+                _button.onClick?.Invoke();
+            }
+        }
+    }
+}
