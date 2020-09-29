db60881a9bc57cfcfdab0021c8cab5fcbd5ea8cb
Kirill Smirnov <kirill@dg-line.ru>
2020-09-29 16:20:11 +0300

    Add Enum Extensions

---
+++ Runtime/UnityUtils/EnumExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UnityUtils
+{
+    public static class EnumExtensions 
+    {
+        public static T Next<T>(this T src) where T : Enum
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException($"Argument {typeof(T).FullName} is not an Enum");
+            }
+
+            var values = (T[])Enum.GetValues(src.GetType());
+            var j = Array.IndexOf(values, src) + 1;
+            return (values.Length==j) ? values[0] : values[j];            
+        }
+    }
+}
