8fd1ac5db0f112ed2975d52823247527685860db
Kirill Smirnov <kirill@dg-line.ru>
2020-10-20 17:21:16 +0300

    v. 1.7.3. Add UiFadePanel.cs

---
+++ Runtime/UnityUtils/VisualEffects/UiFadePanel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityUtils.VisualEffects
+{
+    public class UiFadePanel : MonoBehaviour
+    {
+        private LazyFade _fade;
+        private void Awake()
+        {
+            _fade = transform.GetChild(0).GetComponent<LazyFade>();
+        }
+
+        public void Show(Action finishCallback = null)
+        {
+            _fade.gameObject.SetActive(true);
+            _fade.SetVisibility(true, finishCallback);
+        }
+
+        public void Hide(Action finishCallback = null)
+        {
+            _fade.SetVisibility(false, () =>
+            {
+                _fade.gameObject.SetActive(false);
+                finishCallback?.Invoke();
+            });
+        }
+
+        public void UpdateFade() => _fade.UpdateChildren();
+    }
+}
