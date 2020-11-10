0ebfd96627429661ed95934a13a0799ec858122c
Kirill Smirnov <kirill@dg-line.ru>
2020-11-10 11:02:56 +0300

    Add UiFadePanelEditor.cs to UU

---
+++ Editor/UnityUtils/View/UiFadePanelEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityUtils.VisualEffects;
+
+namespace UnityUtils.View
+{
+    [CustomEditor(typeof(UiFadePanel))]
+    public class UiFadePanelEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var panel = (UiFadePanel) target;
+            
+            if(GUILayout.Button("Show")) panel.Show();
+            if(GUILayout.Button("Hide")) panel.Hide();
+        }
+    }
+}
