2fbfa1c611237e436ac3214516022bef603be8d2
Kirill Smirnov <kirill@dg-line.ru>
2020-11-10 17:58:18 +0300

    Add TMProButton.cs

---
+++ Editor/UnityUtils/View/TMProButtonEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityUtils.View
+{
+    [CustomEditor(typeof(TMProButton))]
+    public class TMProButtonEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            if (GUILayout.Button("change interactable status"))
+            {
+                var t = (TMProButton) target;
+                t.Interactable = !t.Interactable;
+            }
+            base.OnInspectorGUI();
+        }
+    }
+}
---
+++ Runtime/UnityUtils/View/TMProButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityUtils.View
+{
+    public class TMProButton : Button
+    {
+        private TextMeshProUGUI[] _text;
+        private Color[] _defaultColors;
+        protected override void Awake()
+        {
+            base.Awake();
+            InitTextComponents();
+        }
+
+        private void InitTextComponents()
+        {
+            _text = GetComponentsInChildren<TextMeshProUGUI>();
+            if (_text == null || _text.Length == 0) return;
+            _defaultColors = _text.Select(x => x.color).ToArray();
+        }
+
+        public bool Interactable
+        {
+            get => interactable;
+            set
+            {
+                var colorBlock = colors;
+                interactable = value;
+                for (var i = 0; i < _text.Length; i++)
+                {
+                    _text[i].color = interactable ? _defaultColors[i] : _defaultColors[i] + colorBlock.disabledColor;
+                }
+            }
+        }
+    }
+}
