290fde1215c90afc549bac5b76d028f2ff23d2a2
Kirill Smirnov <kirill@dg-line.ru>
2020-11-25 16:29:07 +0300

    XVariable: Add OnChange callback

--- Runtime/UnityUtils/Variables/XVariable.cs
+++ Runtime/UnityUtils/Variables/XVariable.cs
@@ -1,12 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace UnityUtils.Variables
 {
     // [CreateAssetMenu(fileName = "New X Variable", menuName = "Variables/X Variable", order = 0)]
     public abstract class XVariable<T>: ScriptableObject
     {
-        public T value;
-        public static implicit operator T(XVariable<T> v) => v.value;
-        public override string ToString() => value.ToString();
+        public event Action<T> OnChange;
+        
+#pragma warning disable 0649
+        [SerializeField] private T value;
+#pragma warning restore 0649
+
+        private void OnValidate() => OnChange?.Invoke(value);
+
+        public T Value
+        {
+            get => value;
+            set
+            {
+                if(value.Equals(this.value)) return;
+                this.value = value;
+                OnChange?.Invoke(this.value);
+            }
+        }
+        public static implicit operator T(XVariable<T> v) => v.Value;
+        public override string ToString() => Value.ToString();
     }
 }
