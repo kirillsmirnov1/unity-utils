bce9f3db231cd51906f601cb00fcb1c1a0930337
Kirill Smirnov <kirill@dg-line.ru>
2020-11-26 17:18:54 +0300

    Add ULongVariable.cs

---
+++ Runtime/UnityUtils/Variables/ULongVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(fileName = "New ULong Variable", menuName = "Variables/ULong Variable", order = 0)]
+    public class ULongVariable : XVariable<ulong>
+    {
+        
+    }
+}
