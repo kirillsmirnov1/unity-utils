a90a3b8dd371a3d2a59882ba993ef816561e0146
Kirill Smirnov <kirill@dg-line.ru>
2020-12-09 13:58:35 +0300

    Add MathExtensions.cs with RadianToVector2 & DegreeToVector2

---
+++ Runtime/UnityUtils/Extensions/MathExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Packages.UnityUtils.Extensions
+{
+    public class MathExtensions
+    {
+        public static Vector2 RadianToVector2(float radian)
+        {
+            return new Vector2(Mathf.Cos(radian), Mathf.Sin(radian));
+        }
+  
+        public static Vector2 DegreeToVector2(float degree)
+        {
+            return RadianToVector2(degree * Mathf.Deg2Rad);
+        }
+    }
+}
