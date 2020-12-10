388fbbc3f7c7d63a09a94f5dd03674b5e5685d37
Kirill Smirnov <kirill@dg-line.ru>
2020-12-10 15:55:49 +0300

    FloatVariableDisplay.cs

---
+++ Runtime/UnityUtils/Variables/Display/FloatVariableDisplay.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables.Display
+{
+    public class FloatVariableDisplay : XVariableDisplay<float>
+    {
+#pragma warning disable 0649
+        [SerializeField] private FloatVariable variable;
+#pragma warning restore 0649
+        protected override XVariable<float> Variable => variable;
+    }
+}
