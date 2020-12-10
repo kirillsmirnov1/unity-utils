fa375b284338247a7be975b75decdb037a2aae01
Kirill Smirnov <kirill@dg-line.ru>
2020-12-10 12:19:11 +0300

    Add GameEvent.cs and GameEventListener.cs

---
+++ Runtime/UnityUtils/Events/GameEvent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityUtils.Events
+{
+    [CreateAssetMenu(fileName = "New Game Event", menuName = "Events/Game Event")]
+    public class GameEvent : ScriptableObject
+    {
+        /// <summary>
+        /// The list of listeners that this event will notify if it is raised.
+        /// </summary>
+        private readonly List<GameEventListener> _eventListeners = 
+            new List<GameEventListener>();
+
+        public void Raise()
+        {
+            for (var i = _eventListeners.Count - 1; i >= 0; i--)
+            {
+                _eventListeners[i].OnEventRaised();
+            }
+        }
+
+        public void RegisterListener(GameEventListener listener)
+        {
+            if (!_eventListeners.Contains(listener))
+            {
+                _eventListeners.Add(listener);
+            }
+        }
+
+        public void UnregisterListener(GameEventListener listener)
+        {
+            if (_eventListeners.Contains(listener))
+            {
+                _eventListeners.Remove(listener);
+            }
+        }
+    }
+}
---
+++ Runtime/UnityUtils/Events/GameEventListener.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace UnityUtils.Events
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        [Tooltip("Event to register with.")]
+        public GameEvent gameEvent;
+
+        [Tooltip("Response to invoke when Event is raised.")]
+        public UnityEvent response;
+
+        private void OnEnable()
+        {
+            gameEvent.RegisterListener(this);
+        }
+
+        private void OnDisable()
+        {
+            gameEvent.UnregisterListener(this);
+        }
+
+        public void OnEventRaised()
+        {
+            response.Invoke();
+        }
+    }
+}
