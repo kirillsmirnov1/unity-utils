0bdaff18c6d3c41850f228e35f50e92bbcf0df67
Kirill Smirnov <thegaldren@gmail.com>
2020-12-11 13:40:24 +0300

    XVariableDisplay.cs has postfix/prefix fields

--- Runtime/UnityUtils/Variables/Display/XVariableDisplay.cs
+++ Runtime/UnityUtils/Variables/Display/XVariableDisplay.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 {
     public abstract class XVariableDisplay <T> : MonoBehaviour
     {
+#pragma warning disable 0649
+        [SerializeField] private string prefix;
+        [SerializeField] private string postfix;
+#pragma warning restore 0649
+        
         protected abstract XVariable<T> Variable { get; }
 
         private TextMeshProUGUI _text;
@@ -19,7 +24,7 @@ using UnityEngine;
 
         private void Start() => SetText(Variable);
 
-        private void SetText(T val) => _text.text = val.ToString();
+        private void SetText(T val) => _text.text = prefix + val.ToString() + postfix;
 
         private void OnChange(T balance) => SetText(balance);
     }
