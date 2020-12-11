da99a84fb278e2177816d8782a17037ec092cbef
Kirill Smirnov <thegaldren@gmail.com>
2020-12-11 16:21:31 +0300

    XArrayVariable.cs

---
+++ Runtime/UnityUtils/Variables/XArrayVariable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UnityUtils.Variables
+{
+    // [CreateAssetMenu(fileName = "New X Array Variable", menuName = "Variables/X Array Variable", order = 0)]
+    public abstract class XArrayVariable<T> : XVariable<T[]>
+    {
+        /// <summary>
+        /// index, new value
+        /// </summary>
+        public Action<int, T> OnEntryChange;
+        
+        public T this[int i]
+        {
+            get => Value[i];
+            set
+            {
+                if(Value[i].Equals(value)) return;
+                Value[i] = value;
+                OnEntryChange?.Invoke(i, value);
+            }
+        }
+    }
+}
