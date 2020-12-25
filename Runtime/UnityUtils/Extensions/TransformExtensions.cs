0894fa9f9b056457fa1d4301a70a456bc6ceab11
Kirill Smirnov <thegaldren@gmail.com>
2020-12-25 18:48:43 +0300

    Add TransformExtensions.DestroyChildren()

---
+++ Runtime/UnityUtils/Extensions/TransformExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Packages.UnityUtils.Extensions
+{
+    public static class TransformExtensions
+    {
+        public static void DestroyChildren(this Transform transform)
+        {
+            for (var i = 0; i < transform.childCount; i++)
+            {
+                Object.Destroy(transform.GetChild(i).gameObject);
+            }
+        }
+    }
+}
