eabfbea3ef878d1f39ae026b2ae0d1ef48434256
Kirill Smirnov <thegaldren@gmail.com>
2021-01-09 11:56:57 +0300

    Vector2Variable.cs and Vector3Variable.cs

---
+++ Runtime/UnityUtils/Variables/Vector2Variable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(fileName = "New Vector2 Variable", menuName = "Variables/Vector2 Variable", order = 0)]
+    public class Vector2Variable : XVariable<Vector2>
+    {
+        
+    }
+}
---
+++ Runtime/UnityUtils/Variables/Vector3Variable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(fileName = "New Vector3 Variable", menuName = "Variables/Vector3 Variable", order = 0)]
+    public class Vector3Variable : XVariable<Vector3>
+    {
+        
+    }
+}
