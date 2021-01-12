14d552ce641c67a95c1ac22579f30632b3ddbf98
Kirill Smirnov <thegaldren@gmail.com>
2021-01-12 14:46:31 +0300

    Add PersistentGameObjectHolder

---
+++ Runtime/UnityUtils/PersistentGameObjectHolder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace UnityUtils
+{
+    public class PersistentGameObjectHolder : MonoBehaviour
+    {
+        private static bool _instanceExists;
+
+        private void Awake()
+        {
+            if (_instanceExists)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _instanceExists = true;
+                DontDestroyOnLoad(this);
+                for (var i = 0; i < transform.childCount; i++)
+                {
+                    transform.GetChild(i).gameObject.SetActive(true);
+                }
+            }
+        }
+    }
+}
