146d9416cf28cc27e558882b2f9ab20424d98420
Kirill Smirnov <thegaldren@gmail.com>
2021-01-13 15:08:37 +0300

    Add VolumeToggle.cs

---
+++ Runtime/UnityUtils/View/VolumeToggle.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityUtils.Variables;
+
+namespace UnityUtils.View
+{
+    public class VolumeToggle : MonoBehaviour
+    {
+#pragma warning disable 0649
+        [SerializeField] private BoolVariable soundIsOn;
+        [SerializeField] private Toggle toggle;
+        [SerializeField] private Image background;
+#pragma warning restore 0649
+
+        private void Awake()
+        {
+            toggle.onValueChanged.AddListener(OnToggleValueChange);
+            soundIsOn.OnChange += SetToggleValue;
+        }
+
+        private void OnDestroy()
+        {
+            toggle.onValueChanged.RemoveListener(OnToggleValueChange);
+            soundIsOn.OnChange -= SetToggleValue;
+        }
+
+        private void Start() => SetToggleValue(soundIsOn);
+
+        private void SetToggleValue(bool newValue)
+        {
+            if(toggle.isOn == newValue) return;
+            background.enabled = !newValue;
+            toggle.isOn = newValue;
+        }
+
+        private void OnToggleValueChange(bool newValue)
+        {
+            if(newValue == soundIsOn) return;
+            background.enabled = !newValue;
+            soundIsOn.Value = newValue;
+        }
+    }
+}
