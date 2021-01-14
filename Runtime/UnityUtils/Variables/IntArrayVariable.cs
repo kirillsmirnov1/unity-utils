2d0d586a83940399d5eac683bee603e51f5d6d50
Kirill Smirnov <thegaldren@gmail.com>
2021-01-14 13:23:09 +0300

    SpriteArrayVariable.cs & IntArrayVariable.cs

---
+++ Runtime/UnityUtils/Variables/IntArrayVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(fileName = "New Int Array Variable", menuName = "Variables/Int Array Variable", order = 0)]
+    public class IntArrayVariable : XArrayVariable<int>
+    {
+        
+    }
+}
---
+++ Runtime/UnityUtils/Variables/SpriteArrayVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(fileName = "New Sprite Array Variable", menuName = "Variables/Sprite Array Variable", order = 0)]
+    public class SpriteArrayVariable : XArrayVariable<Sprite>
+    {
+        
+    }
+}
