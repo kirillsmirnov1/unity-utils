be3b6b13a512a7922251c895b6b1ac7d4caa68ba
Kirill Smirnov <thegaldren@gmail.com>
2021-01-15 13:06:11 +0300

    V2IntVariable.cs and V2IntArrayVariable.cs

---
+++ Runtime/UnityUtils/Variables/V2IntArrayVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(fileName = "New Vector2Int Array Variable", menuName = "Variables/Vector2Int Array Variable", order = 0)]
+    public class V2IntArrayVariable : XArrayVariable<Vector2Int>
+    {
+        
+    }
+}
---
+++ Runtime/UnityUtils/Variables/V2IntVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(fileName = "New Vector2Int Variable", menuName = "Variables/Vector2Int Variable", order = 0)]
+    public class V2IntVariable : XVariable<Vector2Int>
+    {
+        
+    }
+}
