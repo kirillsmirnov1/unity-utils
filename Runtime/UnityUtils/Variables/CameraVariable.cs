e618f964fb3d160cafda5be2ce25ba3b0abd2105
Kirill Smirnov <thegaldren@gmail.com>
2021-01-26 15:41:40 +0300

    CameraVariable.cs

---
+++ Runtime/UnityUtils/Variables/CameraVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(fileName = "New CameraVariable", menuName = "Variables/Camera Variable", order = 0)]
+    public class CameraVariable : XVariable<UnityEngine.Camera>
+    {
+        
+    }
+}
