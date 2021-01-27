f8b178eca272ef905c35296ded2cf28480c4248e
Kirill Smirnov <thegaldren@gmail.com>
2021-01-27 14:05:21 +0300

    Add SceneLoader.cs

---
+++ Runtime/UnityUtils/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityUtils
+{
+    public class SceneLoader : MonoBehaviour
+    {
+#pragma warning disable 0649
+        [SerializeField] private int sceneIndex;
+#pragma warning restore 0649
+
+        public void Load() => SceneManager.LoadScene(sceneIndex);
+    }
+}
