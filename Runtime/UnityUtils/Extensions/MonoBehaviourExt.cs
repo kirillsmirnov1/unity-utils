a49b52f4b556cb05c01f6ff14c6183dcdc2b137a
Kirill Smirnov <thegaldren@gmail.com>
2021-01-28 12:44:41 +0300

    MonoBehaviourExt.cs with DelayAction()

---
+++ Runtime/UnityUtils/Extensions/MonoBehaviourExt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Packages.UnityUtils.Extensions
+{
+    public static class MonoBehaviourExt
+    {
+        public static void DelayAction(this MonoBehaviour mono, float delay, Action action)
+        {
+            mono.StartCoroutine(DelayCoroutine());
+            
+            IEnumerator DelayCoroutine()
+            {
+                yield return new WaitForSeconds(delay);
+                action?.Invoke();
+            }
+        }
+    }
+}
