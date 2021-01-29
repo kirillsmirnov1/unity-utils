6dc88a37a118f6cf68dba615469be8cd7d2349f9
Kirill Smirnov <thegaldren@gmail.com>
2021-01-29 12:52:31 +0300

    Add VolumeManager.cs

---
+++ Runtime/UnityUtils/Audio/VolumeManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityUtils.Variables;
+
+namespace UnityUtils.Audio
+{
+    public class VolumeManager : MonoBehaviour
+    {
+#pragma warning disable 0649
+        [SerializeField] private BoolVariable volumeOn;
+        [SerializeField] private AudioMixer audioGroup;
+#pragma warning restore 0649
+
+        private void Start() => SetVolume(volumeOn);
+        private void Awake() => volumeOn.OnChange += SetVolume;
+        private void OnDestroy() => volumeOn.OnChange -= SetVolume;
+        private void SetVolume(bool volume) => audioGroup.SetFloat("Volume", volume ? 0 : -80);
+    }
+}
