4148f1df2206b38558389d92e0d3f938c8bb3a7b
Kirill Smirnov <thegaldren@gmail.com>
2021-02-02 11:56:42 +0300

    UIntVariable.cs and UIntVariableDisplay.cs

---
+++ Runtime/UnityUtils/Variables/Display/UIntVariableDisplay.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables.Display
+{
+    public class UIntVariableDisplay : XVariableDisplay<uint>
+    {
+#pragma warning disable 0649
+        [SerializeField] private UIntVariable variable;
+#pragma warning restore 0649
+        protected override XVariable<uint> Variable => variable;
+    }
+}
---
+++ Runtime/UnityUtils/Variables/UIntVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(fileName = "New UInt Variable", menuName = "Variables/UInt Variable", order = 0)]
+    public class UIntVariable : XVariable<uint>
+    {
+        
+    }
+}
