ae7a98cf7c0adb9c0dca3dba7a724411667cc741
Kirill Smirnov <thegaldren@gmail.com>
2021-02-09 19:33:51 +0300

    FrameRate.cs

---
+++ Runtime/UnityUtils/FrameRate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace UnityUtils
+{
+    public class FrameRate : MonoBehaviour
+    {
+#pragma warning disable 0649
+        [SerializeField] private int frameRate = -1;
+#pragma warning restore 0649
+
+        private void Awake() => SetFrameRate();
+
+        private void OnValidate() => SetFrameRate();
+
+        private void SetFrameRate() => Application.targetFrameRate = frameRate;
+    }
+}
