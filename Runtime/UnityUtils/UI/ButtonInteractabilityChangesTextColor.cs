42550a8d24a394d8871fea810727e30054b46112
Kirill Smirnov <thegaldren@gmail.com>
2021-02-25 16:15:21 +0300

    Add ButtonInteractabilityChangesTextColor.cs

---
+++ Runtime/UnityUtils/UI/ButtonInteractabilityChangesTextColor.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityUtils.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class ButtonInteractabilityChangesTextColor : MonoBehaviour
+    {
+        protected TextMeshProUGUI Text;
+        protected Button Button;
+        protected ColorBlock ButtonColors;
+        
+        protected virtual void Awake()
+        {
+            Text = GetComponentInChildren<TextMeshProUGUI>();
+            Button = GetComponent<Button>();
+            ButtonColors = Button.colors;
+        }
+
+        public virtual void SetInteractable(bool interactable)
+        {
+            Button.interactable = interactable;
+            Text.color = interactable ? ButtonColors.normalColor : ButtonColors.disabledColor;
+        } 
+    }
+}
