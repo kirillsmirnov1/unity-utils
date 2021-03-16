67d4a9fdaa8dd0f4130cfa8cdbdfd0ee5341b8ae
Kirill Smirnov <thegaldren@gmail.com>
2021-03-17 02:07:36 +0300

    XVariableInject.cs

---
+++ Runtime/UnityUtils/Variables/Inject/XVariableInject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables.Inject
+{
+    public class XVariableInject<T> : XVariableInjectRoot
+    {
+        [SerializeField] private XVariable<T> variable;
+        protected override void InjectValue() => variable.Value = GetComponent<T>();
+    }
+}
---
+++ Runtime/UnityUtils/Variables/Inject/XVariableInjectRoot.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables.Inject
+{
+    public abstract class XVariableInjectRoot : MonoBehaviour
+    {
+        protected virtual void Awake() => InjectValue();
+        protected abstract void InjectValue();
+    }
+}
