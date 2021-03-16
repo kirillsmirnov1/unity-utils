67effb253ff313e7d147714713a2a632426e0259
Kirill Smirnov <thegaldren@gmail.com>
2021-03-17 02:07:52 +0300

    CanvasVariable.cs

---
+++ Runtime/UnityUtils/Variables/XVariableImpl/CanvasVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(fileName = "New Canvas Variable", menuName = "Variables/Canvas Variable", order = 0)]
+    public class CanvasVariable : XVariable<Canvas>
+    {
+        
+    }
+}
