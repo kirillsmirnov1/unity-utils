9c33aacac2f82512543dd284c3cee2ae1bb2b24a
Kirill Smirnov <thegaldren@gmail.com>
2021-03-17 02:30:47 +0300

    Add comment

--- Runtime/UnityUtils/Variables/Inject/XVariableInjectRoot.cs
+++ Runtime/UnityUtils/Variables/Inject/XVariableInjectRoot.cs
@@ -2,6 +2,7 @@
 
 namespace UnityUtils.Variables.Inject
 {
+    // Non-generic class required to set script execution order
     public abstract class XVariableInjectRoot : MonoBehaviour
     {
         protected virtual void Awake() => InjectValue();
