8b3eb65fd6e2dfed37412068ce081842a582131e
Kirill Smirnov <thegaldren@gmail.com>
2021-03-17 02:50:19 +0300

    Fix GameObjectVariableInject.cs

--- Runtime/UnityUtils/Variables/Inject/InjectImpl/GameObjectVariableInject.cs
+++ Runtime/UnityUtils/Variables/Inject/InjectImpl/GameObjectVariableInject.cs
@@ -4,6 +4,6 @@ namespace UnityUtils.Variables.Inject
 {
     public class GameObjectVariableInject : XVariableInject<GameObject>
     {
-        
+        protected override void InjectValue() => variable.Value = gameObject;
     }
 }
--- Runtime/UnityUtils/Variables/Inject/XVariableInject.cs
+++ Runtime/UnityUtils/Variables/Inject/XVariableInject.cs
@@ -4,7 +4,9 @@ namespace UnityUtils.Variables.Inject
 {
     public class XVariableInject<T> : XVariableInjectRoot
     {
-        [SerializeField] private XVariable<T> variable;
+        [SerializeField] protected XVariable<T> variable;
+        // Works where T : UnityEngine.Component
+        // Needs to be overriden for other cases
         protected override void InjectValue() => variable.Value = GetComponent<T>();
     }
 }
