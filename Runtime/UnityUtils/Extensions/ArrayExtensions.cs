24b8cf40a49716cfc7e1e7bbca3388b935df32f9
Kirill Smirnov <thegaldren@gmail.com>
2021-03-17 19:00:36 +0300

    ArrayExtensions.cs

---
+++ Runtime/UnityUtils/Extensions/ArrayExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace UnityUtils.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static bool HasElementAtIndex(this Array array, int index)
+            => index >= 0 && index < array.Length;
+    }
+}
