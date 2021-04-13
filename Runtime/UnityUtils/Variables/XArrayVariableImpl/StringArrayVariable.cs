829a551631e65aad5715ba10abd74db07d9644d8
Kirill Smirnov <thegaldren@gmail.com>
2021-04-13 18:38:38 +0300

    StringArrayVariable.cs

---
+++ Runtime/UnityUtils/Variables/XArrayVariableImpl/StringArrayVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(fileName = "New String Array Variable", menuName = "Variables/String Array Variable", order = 0)]
+    public class StringArrayVariable : XArrayVariable<string>
+    {
+        
+    }
+}
