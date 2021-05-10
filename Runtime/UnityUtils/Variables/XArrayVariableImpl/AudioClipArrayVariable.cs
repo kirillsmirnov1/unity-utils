aa993faa5a9e93c448a0be2606e9ecad4f2c50d7
Kirill Smirnov <thegaldren@gmail.com>
2021-05-10 13:12:39 +0300

    AudioClipArrayVariable.cs

---
+++ Runtime/UnityUtils/Variables/XArrayVariableImpl/AudioClipArrayVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(fileName = "New AudioClip Array Variable", menuName = "Variables/AudioClip Array Variable", order = 0)]
+    public class AudioClipArrayVariable : XArrayVariable<AudioClip>
+    {
+        
+    }
+}
