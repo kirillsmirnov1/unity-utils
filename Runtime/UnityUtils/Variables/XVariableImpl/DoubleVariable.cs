3638024763fdb0eff2b3e68af68c5f99534ca2a4
Kirill Smirnov <thegaldren@gmail.com>
2021-05-12 19:08:47 +0300

    DoubleVariable.cs

---
+++ Runtime/UnityUtils/Variables/XVariableImpl/DoubleVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(fileName = "New Double Variable", menuName = "Variables/Double Variable", order = 0)]
+    public class DoubleVariable : XVariable<double>
+    {
+        
+    }
+}
