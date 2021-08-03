c5404373d057e0a067cd7ec52ce3178562feb4a1
Kirill Smirnov <thegaldren@gmail.com>
2021-08-03 13:16:42 +0300

    rename Vector2Int XVars

--- Runtime/UnityUtils/Variables/XArrayVariableImpl/Vector2IntArrayVariable.cs
+++ Runtime/UnityUtils/Variables/XArrayVariableImpl/Vector2IntArrayVariable.cs
@@ -3,7 +3,7 @@
 namespace UnityUtils.Variables
 {
     [CreateAssetMenu(fileName = "New Vector2Int Array Variable", menuName = "Variables/Vector2Int Array Variable", order = 0)]
-    public class V2IntArrayVariable : XArrayVariable<Vector2Int>
+    public class Vector2IntArrayVariable : XArrayVariable<Vector2Int>
     {
         
     }
--- Runtime/UnityUtils/Variables/XVariableImpl/Vector2IntVariable.cs
+++ Runtime/UnityUtils/Variables/XVariableImpl/Vector2IntVariable.cs
@@ -3,7 +3,7 @@
 namespace UnityUtils.Variables
 {
     [CreateAssetMenu(fileName = "New Vector2Int Variable", menuName = "Variables/Vector2Int Variable", order = 0)]
-    public class V2IntVariable : XVariable<Vector2Int>
+    public class Vector2IntVariable : XVariable<Vector2Int>
     {
         
     }
