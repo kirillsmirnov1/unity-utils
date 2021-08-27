f11419d142ac42766649093858acc85a146443c2
Kirill Smirnov <thegaldren@gmail.com>
2021-08-27 18:41:08 +0300

    add todo

--- Runtime/UnityUtils/Variables/Debug/VariableDebugEntry.cs
+++ Runtime/UnityUtils/Variables/Debug/VariableDebugEntry.cs
@@ -2,7 +2,7 @@
 
 namespace UnityUtils.Variables.Debug
 {
-    public abstract class VariableDebugEntry : ListViewEntry<AVariable>
+    public abstract class VariableDebugEntry : ListViewEntry<AVariable> // TODO show SO name 
     {
         protected AVariable Variable;
         public override void Fill(AVariable variable)
