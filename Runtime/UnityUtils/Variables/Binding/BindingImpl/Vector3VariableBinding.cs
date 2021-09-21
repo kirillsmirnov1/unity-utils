aec2307819fd8a615ed3b971573a5a6daa6621a1
Kirill Smirnov <thegaldren@gmail.com>
2021-09-21 13:43:32 +0300

    Vector3VariableBinding.cs

---
+++ Runtime/UnityUtils/Variables/Binding/BindingImpl/Vector3VariableBinding.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables.Binding
+{
+    public class Vector3VariableBinding : XVariableBinding<Vector3>
+    {
+        protected override void BindValue() 
+            => variable.Value = transform.position;
+    }
+}
