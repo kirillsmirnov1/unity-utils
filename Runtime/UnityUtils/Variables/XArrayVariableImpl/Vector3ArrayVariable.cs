7b7092396c8d3c5a9c1ea1d10c8070b6051db47a
Kirill Smirnov <thegaldren@gmail.com>
2021-09-22 16:41:17 +0300

    Vector3ArrayVariable.cs

---
+++ Runtime/UnityUtils/Variables/XArrayVariableImpl/Vector3ArrayVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(menuName = "Variables/Array/Vector3ArrayVariable", fileName = "Vector3ArrayVariable", order = 0)]
+    public class Vector3ArrayVariable : XArrayVariable<Vector3>
+    {
+        
+    }
+}
