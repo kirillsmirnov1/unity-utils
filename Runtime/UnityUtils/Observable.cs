f0b49bb37ccc6cd836b85bd7e51e8e005ed23869
Kirill Smirnov <thegaldren@gmail.com>
2021-09-23 13:51:33 +0300

    Observable.cs

---
+++ Runtime/UnityUtils/Observable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityUtils
+{
+    [Serializable]
+    public struct Observable<T>
+    {
+        public event Action<T> OnChange;
+        
+        [SerializeField] private T value;
+        public T Value
+        {
+            get => value;
+            set
+            {
+                if(value.Equals(this.value)) return;
+                this.value = value;
+                OnChange?.Invoke(value);
+            }
+        }
+
+        public static implicit operator T(Observable<T> observable) 
+            => observable.value;
+    }
+}
