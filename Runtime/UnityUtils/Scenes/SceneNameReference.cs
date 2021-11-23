7b52a33abd7eeb7b61d5ac112ce59e06102e196e
Kirill Smirnov <thegaldren@gmail.com>
2021-11-23 11:43:18 +0300

    SceneNameReference.cs

---
+++ Runtime/UnityUtils/Scenes/SceneNameReference.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEditor;
+
+namespace UnityUtils.Scenes
+{
+    [Serializable]
+    public struct SceneNameReference
+    {
+        public string sceneName;
+#if UNITY_EDITOR
+        public SceneAsset sceneAsset;
+        public void SerializeName()
+        {
+            try
+            {
+                sceneName = sceneAsset.name;
+            }
+            catch (NullReferenceException) { }
+        }
+#endif
+    }
+}
