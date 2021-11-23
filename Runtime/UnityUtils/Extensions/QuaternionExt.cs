d7b907a8f2500e6c609e86621e5fbf790bc075b3
Kirill Smirnov <thegaldren@gmail.com>
2021-11-23 11:41:13 +0300

    QuaternionExt.LookRotation2D

---
+++ Runtime/UnityUtils/Extensions/QuaternionExt.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Extensions
+{
+    public static class QuaternionExt
+    {
+        public static Quaternion LookRotation2D(Vector2 direction, float angleOffset = 0f)
+        {
+            var angle = LookRotation2DAngle(direction, angleOffset);
+            return Quaternion.Euler(0, 0, angle);
+        }
+
+        public static float LookRotation2DAngle(Vector2 direction, float angleOffset = 0f) =>
+            Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + angleOffset;
+    }
+}
