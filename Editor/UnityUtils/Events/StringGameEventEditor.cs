3d524b04f67da753b91b209211cdfacbcc11e87c
Kirill Smirnov <thegaldren@gmail.com>
2021-11-30 13:26:46 +0300

    StringGameEventEditor.cs

---
+++ Editor/UnityUtils/Events/StringGameEventEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityUtils.Events
+{
+    [CustomEditor(typeof(StringGameEvent))]
+    public class StringGameEventEditor : Editor
+    {
+        private string _input = "";
+        
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            GUILayout.BeginHorizontal();
+            {
+                _input = EditorGUILayout.TextField(_input);
+                if (GUILayout.Button("Raise"))
+                {
+                    ((StringGameEvent)target).Raise(_input);
+                }
+            }
+            GUILayout.EndHorizontal();
+        }
+    }
+}
