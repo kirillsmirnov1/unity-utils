4fbfe29e97becc7dfb015cb921a1c08a553d627e
Kirill Smirnov <thegaldren@gmail.com>
2021-12-10 14:02:41 +0300

    TMProLinkOpener.cs

---
+++ Runtime/UnityUtils/View/TMProLinkOpener.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UnityUtils.View
+{
+    /// <summary>
+    /// Opens links clicked on TMPRO<br />
+    /// &lt;link="https://www.example.com"&gt;example&lt;/link&gt;<br />
+    /// Source: https://www.feelouttheform.net/unity3d-links-textmeshpro/
+    /// </summary>
+    [RequireComponent(typeof(TMP_Text))]
+    public class TMProLinkOpener : MonoBehaviour, IPointerClickHandler {
+
+        public void OnPointerClick(PointerEventData eventData) {
+            TMP_Text pTextMeshPro = GetComponent<TMP_Text>();
+            int linkIndex = TMP_TextUtilities.FindIntersectingLink(pTextMeshPro, eventData.position, null);  // If you are not in a Canvas using Screen Overlay, put your camera instead of null
+            if (linkIndex != -1) { // was a link clicked?
+                TMP_LinkInfo linkInfo = pTextMeshPro.textInfo.linkInfo[linkIndex];
+                Application.OpenURL(linkInfo.GetLinkID());
+            }
+        }
+
+    }
+}
