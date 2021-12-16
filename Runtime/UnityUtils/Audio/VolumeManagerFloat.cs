bbb40743f28b8772f1c1ff1ca76a5439fa760a4e
Kirill Smirnov <thegaldren@gmail.com>
2021-12-16 15:35:40 +0300

    VolumeManagerFloat.cs

---
+++ Runtime/UnityUtils/Audio/VolumeManagerFloat.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityUtils.Variables;
+
+namespace UnityUtils.Audio
+{
+    public class VolumeManagerFloat : MonoBehaviour
+    {
+        [SerializeField] private FloatVariable volumeVar;
+        [SerializeField] private AudioMixer audioGroup;
+        [SerializeField] private string volumeParamName = "Volume";
+        
+        private void Awake() 
+            => volumeVar.OnChange += SetVolume;
+        private void OnDestroy() 
+            => volumeVar.OnChange -= SetVolume;
+
+        private void OnEnable() 
+            => SetVolume(volumeVar);
+
+        private void SetVolume(float volume)
+        {
+            audioGroup.SetFloat(volumeParamName, Mathf.Log10(volume) * 20);
+        }
+    }
+}
