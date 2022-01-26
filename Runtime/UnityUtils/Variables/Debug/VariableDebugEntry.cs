74ec710d9d1444eb42348c7f1570d5aae91b7fc3
Kirill Smirnov <thegaldren@gmail.com>
2022-01-26 16:26:30 +0300

    Give names to VariableDebugEntry.cs

--- Runtime/UnityUtils/Variables/Debug/VariableDebugEntry.cs
+++ Runtime/UnityUtils/Variables/Debug/VariableDebugEntry.cs
@@ -1,13 +1,18 @@
-﻿using UnityUtils.View;
+﻿using TMPro;
+using UnityEngine;
+using UnityUtils.View;
 
 namespace UnityUtils.Variables.Debug
 {
     public abstract class VariableDebugEntry : ListViewEntry<AVariable> // TODO show SO name 
     {
+        [SerializeField] private TMP_Text variableName;
+        
         protected AVariable Variable;
         public override void Fill(AVariable variable)
         {
             base.Fill(variable);
+            variableName.text = variable.name;
             Variable = variable;
         }
     }
