f25e7643839f002cd64e55813cff59a697c35842
Kirill Smirnov <thegaldren@gmail.com>
2022-05-02 11:44:19 +0400

    TransformExtensions.GetComponentsInFirstChildrenLayer

--- Runtime/UnityUtils/Extensions/TransformExtensions.cs
+++ Runtime/UnityUtils/Extensions/TransformExtensions.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace UnityUtils.Extensions
 {
@@ -13,5 +14,19 @@ namespace UnityUtils.Extensions
         }
         
         public static bool UpsideDown(this Transform transform) => transform.up.y < 0;
+        
+        public static List<T> GetComponentsInFirstChildrenLayer<T>(this Transform parent) 
+        {
+            var result = new List<T>();
+            for (int i = 0; i < parent.childCount; i++)
+            {
+                var elem = parent.GetChild(i).GetComponent<T>();
+                if (elem != null)
+                {
+                    result.Add(elem);
+                }
+            }
+            return result;
+        }
     }
 }
