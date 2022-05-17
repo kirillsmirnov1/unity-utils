04d8df80d7f6777f3d867d01d62de4459d77497f
Kirill Smirnov <thegaldren@gmail.com>
2022-05-17 16:17:38 +0400

    Extract ArrayVariableInputEntry.cs

---
+++ Runtime/UnityUtils/Variables/Input/ArrayVariableInputEntry.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables.Input
+{
+    public abstract class ArrayVariableInputEntry : MonoBehaviour
+    {
+        protected int Index;
+        protected VariableInputProfile Profile;
+
+        protected void Fill(VariableInputProfile profile, int i)
+        {
+            Profile = profile;
+            Index = i;
+        }
+    }
+}
--- Runtime/UnityUtils/Variables/Input/IntArrayVariableInputEntry.cs
+++ Runtime/UnityUtils/Variables/Input/IntArrayVariableInputEntry.cs
@@ -4,39 +4,36 @@ using UnityEngine.UI;
 
 namespace UnityUtils.Variables.Input
 {
-    public class IntArrayVariableInputEntry : MonoBehaviour
+    public class IntArrayVariableInputEntry : ArrayVariableInputEntry
     {
         [SerializeField] private InputField input;
-        
-        private int _index;
-        private IntArrayVariableInput _parent;
-        private VariableInputProfile _profile;
+
+        private IntArrayVariableInput _parent; // TODO move up 
         private int Value { get; set; } = Int32.MinValue;
         
         public void Fill(IntArrayVariableInput parent, VariableInputProfile profile, int i, int val)
         {
+            base.Fill(profile, i);
             _parent = parent;
-            _profile = profile;
-            _index = i;
-            
+
             if(Value == val) return;
             Value = val;
             input.text = Value.ToString();
         }
 
         public void OnEdit(string str) 
-            => _parent.OnEntryEdit(_index, int.Parse(str));
+            => _parent.OnEntryEdit(Index, int.Parse(str));
 
         public void OnPlusPressed() 
-            => ChangeValue(Value + _profile.intIterationStep);
+            => ChangeValue(Value + Profile.intIterationStep);
 
         public void OnMinusPressed() 
-            => ChangeValue(Value - _profile.intIterationStep);
+            => ChangeValue(Value - Profile.intIterationStep);
 
         private void ChangeValue(int newValue) 
-            => _parent.OnEntryEdit(_index, newValue);
+            => _parent.OnEntryEdit(Index, newValue);
 
         public void OnXPressed() 
-            => _parent.RemoveElement(_index);
+            => _parent.RemoveElement(Index);
     }
 }
