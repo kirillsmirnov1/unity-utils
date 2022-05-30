b330aa3b31e7e68559470ba51c94382c9a478657
Kirill Smirnov <thegaldren@gmail.com>
2022-05-30 13:50:25 +0400

    FlipCanvasGroupVisibility.cs

---
+++ Runtime/UnityUtils/View/FlipCanvasGroupVisibility.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityUtils.Variables;
+
+namespace UnityUtils.View
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class FlipCanvasGroupVisibility : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private BoolVariable flag;
+
+        private void Awake()
+        {
+            SetAlpha(flag);
+            flag.OnChange += SetAlpha;
+        }
+
+        private void OnDestroy()
+        {
+            flag.OnChange -= SetAlpha;
+        }
+
+        private void SetAlpha(bool show)
+        {
+            canvasGroup.alpha = show ? 1f : 0f;
+        }
+    }
+}
