fc0c6d8977f855d1f727cea72d50aff81ce9e813
Kirill Smirnov <thegaldren@gmail.com>
2022-06-27 12:55:27 +0400

    Vector3VariableDisplay.cs

---
+++ Runtime/UnityUtils/Variables/Display/Vector3VariableDisplay.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace UnityUtils.Variables.Display
+{
+    public class Vector3VariableDisplay : XVariableDisplay<Vector3>
+    {
+        [SerializeField] private TextMeshProUGUI varName;
+        [SerializeField] private TextMeshProUGUI x;
+        [SerializeField] private TextMeshProUGUI y;
+        [SerializeField] private TextMeshProUGUI z;
+
+        protected void Start()
+        {
+            varName.text = variable.name;
+        }
+
+        protected override void SetText(Vector3 value)
+        {
+            x.text = value.x.ToString(format);
+            y.text = value.y.ToString(format);
+            z.text = value.z.ToString(format);
+        }
+    }
+}
