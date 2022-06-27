2e344ecbc18ff124173dde127ca1c3c06459f2b1
Kirill Smirnov <thegaldren@gmail.com>
2022-06-27 13:17:41 +0400

    VideoPlayerExt.cs

---
+++ Runtime/UnityUtils/Extensions/VideoPlayerExt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace UnityUtils.Extensions
+{
+    public static class VideoPlayerExt
+    {
+        public static IEnumerator ResetAndPlay(this VideoPlayer vp, GameObject renderer)
+        {
+            vp.Reset();
+
+            while (!vp.isPrepared)
+            {
+                yield return null;
+            }
+            
+            vp.Play();
+            yield return new WaitForEndOfFrame();
+            renderer.SetActive(true);
+            while (vp.isPlaying)
+            {
+                yield return null;
+            }
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        public static void Reset(this VideoPlayer vp)
+        {
+            vp.frame = 0;
+            vp.Prepare();
+        }
+    }
+}
