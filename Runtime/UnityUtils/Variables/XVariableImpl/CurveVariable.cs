4ddca8103a62483f45b8402f96f751ae2104a5f5
Kirill Smirnov <thegaldren@gmail.com>
2022-06-27 12:58:15 +0400

    CurveVariable.cs

---
+++ Runtime/UnityUtils/Variables/XVariableImpl/CurveVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UnityUtils.Variables
+{
+    [CreateAssetMenu(menuName = "Variables/CurveVariable", fileName = "CurveVariable", order = 0)]
+    public class CurveVariable : XVariable<AnimationCurve>
+    {
+        
+    }
+}
